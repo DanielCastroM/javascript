31d54cca9407e9a1c4db2d3585a52feb29f06221
DanielCastroM <idanielcastro12@gmail.com>
2022-08-18 22:57:27 -0500

    AVANCE DE METODOS

--- js/buses.js
+++
@@ -1,6 +0,0 @@
-let stockbuses = [
-    { id: 1, nombre: "Bus", cantidad: 49, precio: 1400 },
-    { id: 2, nombre: "MiniBus", cantidad: 33, precio: 800 },
-    { id: 3, nombre: "BusLarge", cantidad: 60, precio: 2000 },
-    { id: 4, nombre: "Bus2Pisos", cantidad: 75, precio: 2500 }
-]
